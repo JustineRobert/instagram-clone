11fee6d9d02d30cdcd33a7866f8d48b959cdd868
Igune Justine Robert <igunejustinerobert@gmail.com>
2020-08-21 23:17:27 +0300

    Add files via upload

---
+++ instagram/modules/Bio.js
@@ -0,0 +1,12 @@
+const Bio =() =>`
+  <section class="bio">
+  <div class="profile-photo">
+    <img src="https://scontent.febb2-1.fna.fbcdn.net/v/t1.0-1/p240x240/107916396_2984101141638257_1422923752609944009_o.jpg?_nc_cat=110&_nc_sid=dbb9e7&_nc_eui2=AeGwb4jeZaKZQEEXjGyn4u_XvDJLoACEhre8MkugAISGt8uj3hK-rUYs7p2uPWUbF2h8XZTtjXwbblJP2gnt3UPj&_nc_ohc=8wZ89KwhUDkAX9WqRU4&_nc_ht=scontent.febb2-1.fna&_nc_tp=6&oh=e6554f166b9fcdbab4bde0855eed0d18&oe=5F60EB06" alt="profile-photo">
+  </div>
+  <div class="profile-info">
+    <p class="name">Igune Justine Robert</p>
+    <button>follow</button>
+  </div>
+  </section>
+`
+export default Bio;
---
+++ instagram/modules/Gallery.js
@@ -0,0 +1,28 @@
+const Gallery =() =>`
+<button>
+  <input type="file" name="photo" id="addPhotoInput">
+  <label for="addPhotoInput"></label>
+<i class="add-photo fas fa-plus-square"></i>
+  <div class="gallery-nav"></div>
+  <section class="gallery">
+    <a href="https://images.pexels.com/photos/2388361/pexels-photo-2388361.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="item">
+      <img src="https://images.pexels.com/photos/2388361/pexels-photo-2388361.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+    </a>
+    <a href="https://images.pexels.com/photos/5010780/pexels-photo-5010780.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="item">
+      <img src="https://images.pexels.com/photos/5010780/pexels-photo-5010780.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+    </a>
+    <a href="https://images.pexels.com/photos/2412941/pexels-photo-2412941.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="item">
+      <img src="https://images.pexels.com/photos/2412941/pexels-photo-2412941.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+    </a>
+    <a href="https://images.pexels.com/photos/3728598/pexels-photo-3728598.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="item">
+      <img src="https://images.pexels.com/photos/3728598/pexels-photo-3728598.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+    </a>
+    <a href="https://images.pexels.com/photos/4673428/pexels-photo-4673428.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="item">
+      <img src="https://images.pexels.com/photos/4673428/pexels-photo-4673428.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+    </a>
+    <a href="#" class="item">
+      <img src="https://images.pexels.com/photos/1769339/pexels-photo-1769339.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+    </a>
+  </section>
+`
+export default Gallery;
